78cce4d2e2733bf26f656dbce27d3c702a6f111d
Dario <me@dgrp.es>
2026-02-05 12:02:37 +0400

    brew: update to v0.2.0

--- Formula/tgcli.rb
+++ Formula/tgcli.rb
@@ -1,28 +1,28 @@
 class Tgcli < Formula
   desc "Telegram CLI tool using grammers (pure Rust MTProto)"
   homepage "https://github.com/dgrr/tgcli"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/dgrr/tgcli/releases/download/v#{version}/tgcli-darwin-arm64"
-      sha256 "PLACEHOLDER_DARWIN_ARM64"
+      sha256 "1378f4f2842037a5e70096d514f925d2d7546186903699bd9985dcd11c5feabc"
     end
     on_intel do
       url "https://github.com/dgrr/tgcli/releases/download/v#{version}/tgcli-darwin-amd64"
-      sha256 "PLACEHOLDER_DARWIN_AMD64"
+      sha256 "d30e1d85103594839e72cc53e5f8b5d5fcab15130da7e18e509d471c06ac38c2"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/dgrr/tgcli/releases/download/v#{version}/tgcli-linux-arm64"
-      sha256 "PLACEHOLDER_LINUX_ARM64"
+      sha256 "6f84a3dff99bf1eddfe2db1bfe6311849a43fcae33f505882d33071d40391ed9"
     end
     on_intel do
       url "https://github.com/dgrr/tgcli/releases/download/v#{version}/tgcli-linux-amd64"
-      sha256 "PLACEHOLDER_LINUX_AMD64"
+      sha256 "bcba33db43a2c86b63f7e0baa59b61c0965378b02058e7d444ea3a0d8e8a7ed5"
     end
   end
 
